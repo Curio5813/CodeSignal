d9c8688ed0e48d8961370bfa53bc524d0a2bd38d
Curio5813 <marcosglasner@gmail.com>
2022-11-26 01:07:52 +0000

    Updating files

--- SmoothSailing/reverse_in_parentheses.py
+++ SmoothSailing/reverse_in_parentheses.py
@@ -1,8 +1,9 @@
 inputString = '(foo(bar(baz))blim)'
 
+
 def reverseInParentheses(inputString):
-    """Write a function that reverses characters in (possibly nested) parentheses in the
-    input string."""
+    """Write a function that reverses characters in (possibly nested)
+    parentheses in the input string."""
     start = 0
     s = inputString
     print(s)
